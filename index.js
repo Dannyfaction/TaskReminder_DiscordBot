25f551453e79be18d7308a5f9b613d6c0c2987ae
DESKTOP-1TV7T9L\Danny <dannykruiswijk@hotmail.com>
2021-12-29 00:39:38 +0100

    Add basic Discord bot behavior

--- index.js
+++ index.js
@@ -1,10 +1,28 @@
-const express = require('express')
-const path = require('path')
-const PORT = process.env.PORT || 5000
+const Discord = require('discord.js');
+const client = new Discord.Client();
 
-express()
-  .use(express.static(path.join(__dirname, 'public')))
-  .set('views', path.join(__dirname, 'views'))
-  .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('pages/index'))
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+client.on('ready', () => {
+  console.log(`Logged in...`);
+});
+
+client.on('message', msg => {
+  msg.reply('pong');
+});
+
+client.login('OTI1NTI2MjYzMjk5NDczNDg4.YcuZgA.a5qVEYXCd-F5oWq-pUsgW4-ff0Y');
+
+const prefix = `!`;
+client.on("message", function (message) {
+  if (message.author.bot) return;
+  if (!message.content.startsWith(prefix)) return;
+  // get channel id and command out of message
+  const channelId = message.channel.id;
+  const commandBody = message.content.slice(prefix.length);
+  const args = commandBody.split(" ");
+  const command = args.shift().toLowerCase();
+  if (command === 'play') {
+    ... 
+    // after successfully create the play space, response to the user that call this command.
+    message.reply(...);
+  }
+}
